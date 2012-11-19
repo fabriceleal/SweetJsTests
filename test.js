e20d25a418553c97a3b893d63347326eb38b4b42
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2012-11-19 22:47:40 +0000

    more fun

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 ﻿/*
-	A short cut for function
+	A shortcut for the function keyword.
 */
 
 macro λ {	
@@ -12,23 +12,18 @@ macro λ {
 }
 
 /*
-	Let à là lisp
+	Let à là lisp.
 */
 macro $let {
 	case ( $($id:ident = $val:expr) (,) ... ) $body => { 
-		/*(function(){		
-			$(var $id = $val;) ...
-			var ____r = (function() $body)();
-			return ____r;
-		})()*/
 		(function ($id (,) ...) $body)($val (,) ...)
 	}	
 }
 
 /*
-	Constant combinator - returns a function that returns the same value
+	Constant combinator - returns a function that returns the same value.
 */
-macro $K {	
+macro $konst {	
 	case ($literal) => {
 		(function(){
 			return $literal;
@@ -45,38 +40,169 @@ macro $K {
 		})
 	}
 }
-/**/
 
+/*
+	Assures that a function is called only once; 
+	any other call returns the first return value, regardless of parameters
+*/
+macro $once {
+	case (function $params $body) => {
+		(function(){
+			var excd = false;
+			var r;
+			return function(){
+				if(!excd){
+					excd = true;
+					r = (function $params $body).apply(null, Array.prototype.slice.call(arguments));
+				}				
+				return r;
+			}
+		})()
+	}
+	case (λ $params $body) => {
+		(function(){
+			var excd = false;
+			var r;
+			return function(){
+				if(!excd){
+					excd = true;
+					r = (function $params $body).apply(null, Array.prototype.slice.call(arguments));
+				}				
+				return r;
+			}
+		})()
+	}
+}
+
+/*
+	Macrofied y-combinator >:D
+*/
+macro $y {
+	case ( $var ) => {
+		function(){
+			return function(f){
+				return f(f)
+			}(function(f){
+				return $var(function(x){
+					return f(f)(x);
+				})
+			});
+		}()
+	}
+	case ( function $pars $body ) => {
+		function(){
+			return function(f){
+				return f(f)
+			}(function(f){
+				return function $pars $body(function(x){
+					return f(f)(x);
+				})
+			});
+		}()
+	}
+	case ( λ $pars $body ) => {
+		function(){
+			return function(f){
+				return f(f)
+			}(function(f){
+				return function $pars $body(function(x){
+					return f(f)(x);
+				})
+			});
+		}()
+	}
+}
+
+/*
+	Argument # n
+*/
+macro $a {
+	case ($nbr) => {
+		arguments[$nbr]
+	}
+}
+
+/*
+	Call With Arguments
+*/
+macro $cwa {
+	case ($var) => {
+		$var.apply(null, Array.prototype.slice.call(arguments))
+	}
+	case (function $pars $body) => {
+		function $pars $body.apply(null, Array.prototype.slice.call(arguments))
+	}
+	case (λ $pars $body) => {
+		function $pars $body.apply(null, Array.prototype.slice.call(arguments))
+	}
+};
+
+/*
+	A functional if. Translate this to macro?
+*/
+function fif(Predicate){
+	return {
+		fthen : function(ValueTrue){
+			var wValueTrue = typeof ValueTrue == 'function'? ValueTrue : $konst(ValueTrue);
+			
+			return {
+				felse : function(ValueFalse) {
+					var wValueFalse = typeof ValueFalse == 'function' ? ValueFalse : $konst(ValueFalse);
+					
+					return function(){
+						return $cwa(Predicate) ? $cwa(wValueTrue) : $cwa(wValueFalse);
+					}
+				},
+				noelse : function(){
+					// ...
+				}
+			}
+		},
+		nothen : function(){
+			// ...
+		}
+	}
+};
+
+var f2 = fif(λ () { return $a(0) == $a(1); }).fthen(5).felse(2);
+
+console.log(f2(1, 2));
+
+// z is a function that squares its argument
 var z = λ (b) { return b*b; };
-var w = $K(123);
-var w2 = $K(λ (b) { return b*b; });
 
+// w is a function that always returns 123
+var w = $konst(123);
+
+// w2 is a function that always returns a function that squares its argument
+var w2 = $konst(λ (b) { return b*b; });
+
+// w3 is a function that always returns the same object
+var w3 = $konst({ a: 1, b : 'asd'});
+
+// f is the factorial function
+var f = $y(λ(fact){ return λ(n){ return ( n == 0 ) ? 1 : n * fact(n - 1); }});
+
+// d is a function that executes its arg only once;
+var d = $once(λ(){
+	console.log('called!'); 
+	return 5; 
+});
+
+// v is a computation with new bindings
 var v = $let(a='hello', b='world') {
 	return a + ' ' + b;
 };
+
+// TESTS
+
+console.log(f(5));
+console.log('1st call');
+console.log(d());
+console.log('2nd call');
+console.log(d());
 
 console.log(v);
 console.log(z(5));
 console.log(w());
 console.log(w2()(5));
-
-/*λ print (a) {
-	console.log(a); 
-};*/
-
-/*var a = λ (a) {
-	return (a + 1); 
-};*/
-
-//var f = $K(5);
-
-//var z = $K();
-//console.log($K(λ r { return r*r;} )()(5));
-//console.log(z()(5));
-
-//print(4);
-//console.log(a(2));
-//console.log(λ(a){ return a+a;}(3));
-/*
-
-*/
